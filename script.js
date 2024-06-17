1331a4ef5b05de1ce1341735d136bb01027aa088
Mohamed Bankolé <medbankole97@gmail.com>
2024-06-17 08:09:27 +0000

    test

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// menu barre responsive
 var small_menu = document.querySelector('.toggle_menu')
 var menu = document.querySelector('.nav-links')
 
@@ -5,3 +6,5 @@ small_menu.onclick = function(){
      small_menu.classList.toggle('active');
      menu.classList.toggle('responsive');
 }
+
+// validité des champs input
