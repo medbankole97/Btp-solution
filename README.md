ec94fc4bf973c87dc56d0e0fd63363765cf57c30
Mohamed Bankolé <medbankole97@gmail.com>
2024-06-18 01:20:53 +0000

    modification

---
+++ README.md
@@ -0,0 +1,5 @@
+BTS Solutions est une entreprise spécialisée dans le secteur du BTP, offrant
+une gamme complète de services allant de la construction de bâtiments
+résidentiels à la gestion de grands projets d'infrastructure. Fondée en 2005,
+elle a acquis une solide réputation grâce à la qualité de ses projets et à son
+engagement envers l'excellence.
--- script.js
+++ script.js
@@ -8,6 +8,19 @@ small_menu.onclick = function(){
 }
 
 // validité des champs input
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+     let nom = document.getElementById('nom');
+     let regexNom = /^[a-zA-Z\s]+$/;
+     let erreurNom = document.getElementById('errorName');
+     let valide = true;
 
-
-
+     if (!regexNom.test(nom.value)) {
+         erreurNom.innerHTML = 'votre nom est invalide';
+         valide = false;
+     } else {
+         erreurNom.innerHTML = '';
+     }
+     if (!valide) {
+         event.preventDefault();
+     }
+ });
